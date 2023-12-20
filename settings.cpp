790250422f4732b6fe789476cab6e5554bb3b889
Ali Shahid <129631456+AliShahid20@users.noreply.github.com>
2023-12-19 23:17:56 -0800

    settings.cpp

---
+++ settings.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    bool READ_MASSAGE = true;
+    bool VOICE_REPLY = true;
+    bool AUTO_BIO = true;
+    bool INBOX_BLOCK = true;
+    bool AUTO_REACT = true;
+    bool ANTI_BADWORD = true;
+    bool ANTI_G_LINK = true;
+    bool NUMBER_212_BLOCK = true;
+    bool SEND_WELCOME = true;
+    bool SEND_GOODBYE = true;
+    bool BLOCK_CMD_MESSAGE_SEND = true;
+    bool AUTO_FUNCTION = true;
+    bool BOT_OFFLINE = true;
+    bool ONLY_PREFIX = true;
+    bool UPLOAD_MAX = true;
+    bool DISABLE_INBOX = true;
+    bool DISABLE_INBOX_MESSAGE_SEND = true;
+    bool SEX_VIDEO_DOWNLOAD = true;
+    bool INBOX_ONLY_WORK = true;
+    bool AUTO_OPEN_AI = true;
+    bool PRIVET_BOT = true;
+    bool DISABLE_COMMANDS = true;
+    bool DISABLE_COMMAND_MESSAGE_SEND = true;
+    bool AUTO_SEEN_STATUS = true;
+
+    vector<string> owner = {"+923043942429"};
+    string cap = "© 𝙶𝙴𝙽𝙴𝚁𝙰𝚃𝙴𝙳 𝙱𝚈 𝚀𝚄𝙴𝙴𝙽 𝙴𝙻𝙸𝚂𝙰 ²⁰²³";
+    string botnma = "BOT 3 𝛁";
+    string ownernma = "ᴍ ʀ ⃞ ALI";
+    string packname = "𝙼𝚁 ALI";
+    bool AUTO_SEEN_STATUS = true;
+    string INBOX_BLOCK_MSG = "BLOCK !!!";
+    string OWNER_REACT = "💻";
+    string SONG_DOWN = "⬇️ Downloading Your Song...";
+    string SONG_UP = "⬆️ Uploding Your Song...";
+    string MENU_IMOJI = "👸|ℹ️";
+    string TIME_ZONE = "Asia/Colombo";
+    string REMOVE_BG_API = "apikey get on remoebg.com";
+    string LANG = "EN";
+    string alivelogo = "https://telegra.ph/file/b3f17c16eedf99731a52d.jpg";
+    string ALIVE_MESSAGE = "default";
+    string WELCOME = "";
+    string WELCOME_IMAGE = "";
+    string GOODBYE = "";
+    string GOODBYE_IMAGE = "";
+    vector<string> NO_BLOCK = {"94715166712", "94719574492"};
+    vector<string> IMG_BLOCKER = {"xxx", "sex", "mia"};
+    string CHATGPT_API = "";
+    vector<string> SEX_DOWNLOAD_GROUPS = {"120363147404508506@g.us"};
+    string GROUP_LINK = "https://chat.whatsapp.com/K2AiOg3a34A5mkNtyRsmWc";
+    string PORT = "8000";
+    string zenzapi = "01ABEB1E11";
+    string mess_success = "✅ Done!";
+    string mess_admin = "*Only admins can use this command !*";
+    string mess_botAdmin = "*Please admin the bot first !*";
+    string mess_owner = "*Sorry, this command is only for the owner !*";
+    string mess_group = "*This command can only be used in groups !*";
+    string mess_private = "*This command can only be used in Inbox !*";
+    string mess_bot = "*This command can only be used by bot number !*";
+    string mess_wait = "*♲ Please Wait ...*";
+    string mess_endLimit = "Your Daily Limit Has Expired, The Limit Will Be Reset Every 12 Hours";
+    string mess_BLOCK_CMD_MSG = "*⚠️ This Command is Block By Owner*";
+    string mess_IMG_BLOCK = "This Word is Ban from image downloader";
+    string mess_DISABLE_INBOX = "Inbox DISABLE By Bot Owner Please Only Use Groups";
+    string mess_ONLY_SEX_GROUP = "Adult Video Download Only Allowed On This Group \n\n https://chat.whatsapp.com/JYAg2lMpRTF2bgkeFCxsvf";
+    string mess_DISABLE_CMD = "🚫 Commands Are DISABLE by owner";
+    string mess_BAD_DETECT = "";
+    string mess_BLOCK = "";
+    string mess_UNBLOCK = "";
+    string mess_KICK = "";
+    string mess_ADD = "";
+    string mess_LEAVE = "";
+    string mess_PROMOTE = "";
+    string mess_DEMOTE = "";
+
+    return 0;
+}
+
+
